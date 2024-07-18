a3149ea6fceba282a9467d7434a82453ad3091dd
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-07-18 16:32:42 -0300

    new layout with shadcn/ui

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,14 @@
+import { Poppins as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans  = FontSans ({ 
+  subsets: ["latin"],
+  weight: ['400', '500', '600'],
+  variable: "--font-sans"
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={cn(
+          "min-h-screen bg-foreground font-sans antialiased",
+          fontSans.variable
+        )}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
