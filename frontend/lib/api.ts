acb4be1ad1b7a2d0f2e88e565e01f2378f05a3c4
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-07-19 12:47:34 -0300

    Novos consumos da API e verificação sobre os headers

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -3,35 +3,88 @@ import cookies from "./cookies";
 const HOST = "http://localhost:3333";
 
 const headers = new Headers();
-headers.append("Content-Type", "application/json");
+if (!headers.has("Content-Type"))
+    headers.append("Content-Type", "application/json");
 
 async function signup(email:string, password:string) {
     const body = JSON.stringify({email, password})
-    const response = await fetch(HOST+"/auth/signup", {method: "POST", headers, body})
-    const json = await response.json()
-
-    if (response.ok) {
-        const user = json.user
-        cookies.set("authToken", user.token, 1);
-        return { status: true };
+    try {
+        const response = await fetch(HOST+"/auth/signup", {method: "POST", headers, body})
+        if (response.ok) {
+            const json = await response.json()
+            const user = json.user
+            cookies.set("authToken", user.token, 1);
+            return { status: true };
+        } else {
+            throw new Error(await response.text());
+        }
+    } catch (error:any) {
+        let err: Error = error
+        return { status: false, error: err.message }
     }
-    return { status: false, error: json.error }
 }
 
 async function signin(email:string, password:string) {
     const body = JSON.stringify({email, password})
-    const response = await fetch(HOST+"/auth/login", {method: "POST", headers, body})
-    const json = await response.json()
-
-    if (response.ok) {
-        const user = json.user
-        cookies.set("authToken", user.token, 1);
-        return { status: true };
+    
+    try {
+        const response = await fetch(HOST+"/auth/login", {method: "POST", headers, body})
+        if (response.ok) {
+            const json = await response.json()
+            const user = json.user
+            cookies.set("authToken", user.token, 1);
+            return { status: true };
+        } else {
+            throw new Error(await response.text());
+        }
+    } catch (error:any) {
+        let err: Error = error
+        return { status: false, error: err.message }
     }
-    return { status: false, error: json.error }
+}
+
+async function getHistory() {
+    const token = cookies.get("authToken");
+    if (!headers.has("Authorization"))
+        headers.append("Authorization", "Bearer "+token);
+    try {
+        const response = await fetch(HOST+"/app/history", { method: "GET", headers })
+        if (response.ok) {
+            const json = await response.json()
+            return { status: true, history: json.history };
+        } else {
+            throw new Error(await response.text());
+        }
+    } catch (error:any) {
+        let err: Error = error
+        return { status: false, error: err.message }
+    }
+}
+
+async function getQuestion(uid?:string) {
+    const token = cookies.get("authToken");
+    const PARAMS = uid ? "?uid="+uid : "";
+    if (!headers.has("Authorization"))
+        headers.append("Authorization", "Bearer "+token);
+    try {
+        const response = await fetch(HOST+"/app/question"+PARAMS, 
+            { method: "GET", headers }
+        )
+        if (response.ok) {
+            const json = await response.json()
+            return { status: true, question: json.question };
+        } else {
+            throw new Error(await response.text());
+        }
+    } catch (error:any) {
+        let err: Error = error
+        return { status: false, error: err.message }
+    }
 }
 
 export default {
     signup,
-    signin
+    signin,
+    getHistory,
+    getQuestion
 }
